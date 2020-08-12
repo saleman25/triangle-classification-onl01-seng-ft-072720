565e1af14152e6b019db30bffa8f4c5b95f111bd
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 18:21:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  
+attr_accessor :x, :y, :z 
+  
+def initialize(x, y, z ) 
+  @x = x
+  @y = y
+  @z = z
+ end  
+  
+def kind
+  validate_triangle
+    if x == y && y == z
+    :equilateral
+    elsif x == y || y == z || x == z
+    :isosceles
+    else
+    :scalene
 end
+end
+
+  def validate_triangle
+    real_triangle = [(x + y > z), (x + z > y), (y + z > x)]
+    [x, y, z].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+  
+class TriangleError < StandardError  
+end  
+  
+  
+end  
