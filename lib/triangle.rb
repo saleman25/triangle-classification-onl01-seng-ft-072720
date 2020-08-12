61cdfe24521de48dcbb58823b909fdd6a1a4d03f
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 17:43:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,8 +2,10 @@ class Triangle
   
 attr_accessor :length1 , :length2, :length3  
   
-def initialize(length) 
-  length.each {|key, value| self.send(("#{key}="), value)}
+def initialize(:length1 , :length2, :length3 ) 
+  @length1 = length1
+  @length2 = length2
+  @length3 = length3
  end  
   
 def kind
