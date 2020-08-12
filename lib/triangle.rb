092ba5c28b98dec56a8ae8b7c1467d25c3684d8c
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 17:41:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  
+attr_accessor :length1 , :length2, :length3  
+  
+def initialize(length) 
+  length.each {|key, value| self.send(("#{key}="), value)}
+ end  
+  
+def kind
+:equilateral
+:isosceles
+:scalene
+end 
+  
+class TriangleError < StandardError  
+  
+ end  
+  
+  
 end
