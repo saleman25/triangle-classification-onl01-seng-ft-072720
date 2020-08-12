cee289fbd583d376c70479ae9866f51d46ebe39e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 17:46:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,11 @@
 class Triangle
   
-attr_accessor :length1 , :length2, :length3  
+attr_accessor :x, :y, :z 
   
-def initialize(:length1 , :length2, :length3 ) 
-  @length1 = length1
-  @length2 = length2
-  @length3 = length3
+def initialize(:x, :y, :z ) 
+  @x = x
+  @y = y
+  @z = z
  end  
   
 def kind
